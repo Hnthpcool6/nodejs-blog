b892909926e460eda9eec56c49250aeb9b29cd54
Hnthpcool6 <hnt.vn.vn@gmail.com>
2023-09-09 21:46:02 +0700

    update

--- src/index.js
+++ src/index.js
@@ -6,16 +6,21 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname,'public')))
+
 //HTTP logger
 app.use(morgan('combined'));
 
 //Template engine
-app.engine('handlebars', handlebars.engine({ defaultLayout: 'main', extname: '.handlebars',}));
-app.set('view engine', 'handlebars');
+app.engine('hbs', handlebars.engine({ defaultLayout: 'main', extname: '.hbs',}));
+app.set('view engine', 'hbs');
 
 app.set('views', path.join(__dirname,'resources/views'));
 
 app.get('/', (req, res) => {
+  res.render('home');
+});
+app.get('/news', (req, res) => {
   res.render('home');
 });
 
