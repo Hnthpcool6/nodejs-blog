b2ab48913a08b9c621116027fbf73ee8ebe235f6
Hnthpcool6 <hnt.vn.vn@gmail.com>
2023-09-12 20:50:10 +0700

    switch to MVC

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,18 @@
-
+const newsRouter = require('./news');
+const siteRouter = require('./site');
 function route(app) {
 
-    //get
-    app.get('/', (req, res) => {
-        res.render('home');
-    });
-    app.get('/news', (req, res) => {
-        res.render('news');
-    });
-    app.get('/search', (req, res) => {
-        res.render('search');
-    });
-
-    //post
-    app.post('/search', (req, res) => {
-        res.send('');
-    });
+    app.use('/news', newsRouter);
+    
+    app.use('/', siteRouter);
+    // get
+    // app.get('/news', (req, res) => {
+    //     res.render('news');
+    // });
+    // //post
+    // app.post('/search', (req, res) => {
+    //     res.send('');
+    // });
 
 }
 
